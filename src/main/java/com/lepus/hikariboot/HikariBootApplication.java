583a54f1c0b1e0b5ccafd56e0f303f2d59ce574b
jim <jim@cit.com>
2018-02-03 10:04:28 +0800

    init

---
+++ src/main/java/com/lepus/hikariboot/HikariBootApplication.java
@@ -0,0 +1,12 @@
+package com.lepus.hikariboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikariBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikariBootApplication.class, args);
+	}
+}
